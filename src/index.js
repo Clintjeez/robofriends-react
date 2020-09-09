da20cac0d46f0d7cd5ca8234874c1b52fd3b294a
clintjeez <clintjeez@gmail.com>
2020-09-09 20:43:34 +0100

    card and cardlist

--- src/index.js
+++ src/index.js
@@ -4,14 +4,6 @@ import "./index.css";
 import "tachyons";
 
 import { robots } from "./robots";
+import CardList from "./CardList";
 
-import Card from "./Card";
-
-ReactDOM.render(
-  <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<CardList robots={robots} />, document.getElementById("root"));
