cbcaef62f3f3428e4b0d7e5b2a778114a7fb02ec
clintjeez <clintjeez@gmail.com>
2020-09-18 11:53:15 +0100

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 import CounterButton from "./CounterButton";
 
 class Header extends Component {
-  //   shouldComponentUpdate(nextProps, nextState) {
-  //     return false;
-  //   }
   render() {
     console.log("Header");
     return (
